efdc7f0b08ccb2f33f3fb13ed70f054df056cfb0
Alberto Basaglia <alberto.basaglia17@gmail.com>
2019-05-06 06:17:34 +0200

    added autoupdate on clientweb

--- clientWeb/src/app/api.service.ts
+++ clientWeb/src/app/api.service.ts
@@ -5,7 +5,7 @@ import {HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class ApiService {
-  private baseUrl = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/seriot-xsfbc/service/Check/incoming_webhook/getAll';
+  private baseUrl = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/seriot-xsfbc/service/Check/incoming_webhook/getLast50';
   public records: Map<string, string>[] ;
   public fields: string[];
   public addresses: number[];
@@ -15,33 +15,47 @@ export class ApiService {
     this.fields = [];
     this.addresses = [];
     this.chartFields = [];
-    http.get(this.baseUrl).toPromise().then(
-      (v: Array<any>) => {
-        for ( const  e of v) {
-          const newEl = new Map<string, string>();
-          delete e._id;
-          Object.keys(e).forEach(
-            (key) => {
-              if (this.fields.indexOf(key) === -1) {
-                this.fields.push(key);
-                if (key !== 'addr' && key !== 'time') {
-                  this.chartFields.push(key);
+    setInterval(
+      () => {
+        http.get(this.baseUrl).toPromise().then(
+          (v: Array<any>) => {
+            let recordsNew = [];
+            let fieldsNew = [];
+            let addressesNew = [];
+            let chartFieldsNew = [];
+            for ( const e of v) {
+              const newEl = new Map<string, string>();
+              delete e._id;
+              Object.keys(e).forEach(
+                (key) => {
+                  if (fieldsNew.indexOf(key) === -1) {
+                    fieldsNew.push(key);
+                    if (key !== 'addr' && key !== 'time') {
+                      chartFieldsNew.push(key);
+                    }
+                  }
+                  if (key === 'time') {
+                    newEl.set(key, new Date(+e[key].$numberLong).toTimeString());
+                  } else {
+                    newEl.set(key, e[key].$numberLong);
+                  }
                 }
+              );
+              if ( addressesNew.indexOf(+newEl.get('addr')) === -1) {
+                addressesNew.push(+newEl.get('addr'));
               }
-              if (key === 'time') {
-                newEl.set(key, new Date(+e[key].$numberLong).toTimeString());
-              } else {
-                newEl.set(key, e[key].$numberLong);
-              }
-            }
-          );
-          if ( this.addresses.indexOf(+newEl.get('addr')) === -1) {
-            this.addresses.push(+newEl.get('addr'));
-          }
 
-          this.records.push(newEl);
-        }
-      }
+              recordsNew.push(newEl);
+            }
+            this.records = recordsNew;
+            this.chartFields = chartFieldsNew;
+            this.fields = fieldsNew;
+            this.addresses = addressesNew;
+          }
+        );
+
+
+      }, 1000
     );
   }
 }
